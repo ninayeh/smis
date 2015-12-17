3a27d68fc3ab365e4fe83b241e78c4cb7876cd82
Nina Yeh <ninayeh1211@gmail.com>
2015-12-17 19:29:02 +0800

    Deleted image

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Smis
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
     config.assets.initialize_on_precompile = false
   end
 end
