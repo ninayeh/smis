1b5aa718914d9c81d17d63938b897a3ff2a070a1
Nina Yeh <ninayeh1211@gmail.com>
2016-02-18 00:32:39 +0800

    remove test folder and setup RSpec

--- test/controllers/backend/departments_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Backend::DepartmentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/book_list_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class BookListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
