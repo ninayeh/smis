2b95ee8ef9ef3f246c7ac718d445a645ecb3dbb0
Nina Yeh <ninayeh1211@gmail.com>
2016-02-18 13:29:14 +0800

    Add factories

---
+++ spec/factoties/laboratories.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :laboratory do
+
+  end
+end
---
+++ spec/models/laboratory_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Laboratory do
+  context "新增實驗室" do
+    u = create(:the_laboratory)
+    # expect(Laboratory.count).to be 1
+  end
+
+end
