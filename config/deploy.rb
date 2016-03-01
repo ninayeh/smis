1ff743c3f4601f6ff1b28b7f4c864898ea61e595
Awaru Ryudo <ryudoawaru@gmail.com>
2016-03-01 14:16:54 +0800

    Remove bin from link list

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :deploy_to, '/home/deploy/smis'
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :linked_files, %w{config/database.yml config/settings.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 set :default_env, { path: "$PATH:/usr/local/ruby23/bin:" }
