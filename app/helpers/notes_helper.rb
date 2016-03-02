ff7e11cd468de70aaa6b03f4db098f78e067ebdf
Nina Yeh <ninayeh1211@gmail.com>
2016-03-02 18:35:45 +0800

    新增helper

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -2,4 +2,12 @@ module NotesHelper
   def sanitize_content(resource)
     resource.html_safe
   end
+
+  def check_final_content(resource)
+    if resource.present?
+      '<span class="badge bg-info"><i class=" fa fa-check"></i></span>'.html_safe
+    else
+      '<span class="badge bg-important"><i class=" fa fa-times "></i></span>'.html_safe
+    end
+  end
 end
