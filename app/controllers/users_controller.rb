39e99230d642cf385bf1dc8137d0a53eb6ea5532
Nina Yeh <ninayeh1211@gmail.com>
2016-03-16 13:00:51 +0800

    顯示目前任務名稱

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update]
+
   def index
-    # @users = User.where(role: 'admin')
     @users = User.all
   end
-  #issue
-  def save
-    if !params[:id].nil?
-      # @user = User.where(role: 'admin').find(params[:id])
-      @user = User.find(params[:id])
-    else
-      redirect_to users_path
-    end
 
-    @schedules = Schedule.find_by(user_id: params[:id])
-    unless @schedules.nil? && @schedules.published?
+  def save
+    @user = User.find(params[:id])
+    @schedules = Schedule.where(user_id: params[:id]).find_by(published: true)
+    unless @schedules.nil?
       @missions = Mission.where(schedule_id: @schedules.id).order(end_date: :asc)
     end
 
@@ -26,8 +20,10 @@ class UsersController < ApplicationController
   def show
     @notes = Note.where(user_id: params[:id]).order(updated_at: :asc)
     @theses = Thesis.where(user_id: params[:id]).order(updated_at: :asc)
-    @schedules = Schedule.find_by(user_id: params[:id])
-
+    @schedules = Schedule.where(user_id: params[:id]).find_by(published: true)
+    unless @schedules.nil?
+      @the_mission = Mission.where(schedule_id: @schedules.id).now_mission.first
+    end
   end
 
   def edit
--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,3 +1,5 @@
 class Mission < ActiveRecord::Base
   belongs_to :schedule
+
+  scope :now_mission, -> { where("DATE(?) BETWEEN start_date AND end_date", Date.today) }
 end
