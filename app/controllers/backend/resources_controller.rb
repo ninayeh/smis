8a92a5144faa22923b3e42f7c1452b3ee012b9ca
Nina Yeh <ninayeh1211@gmail.com>
2016-03-17 13:55:03 +0800

    Fix backend can not find current user issue

--- app/controllers/backend/resources_controller.rb
+++ app/controllers/backend/resources_controller.rb
@@ -1,4 +1,17 @@
 class Backend::ResourcesController < Backend::BaseController
+  def new
+    @resource = Resource.new
+  end
+
+  def create
+    @resource = current_user.resources.new(object_params)
+    @resource.published = false;
+    if @resource.save
+      redirect_to backend_resources_path, notice: '您的分享的資源已送出！'
+    else
+      render :new
+    end
+  end
   private
   def collection_scope
     Resource
