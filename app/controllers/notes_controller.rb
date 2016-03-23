79c64f11165fbf142857e51868c47cf5072ad7ac
Nina Yeh <ninayeh1211@gmail.com>
2016-03-23 15:48:19 +0800

    Add create_date option to note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -75,6 +75,6 @@ class NotesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
       params.require(:note).permit(:title, :category, :content, :final_content,
-                                   :user_id, :published)
+                                   :user_id, :published, :created_at)
     end
 end
