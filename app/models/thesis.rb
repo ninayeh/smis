d70f64d4fb3646c5cbdf5e067a3d59fe852d29bc
Nina Yeh <ninayeh1211@gmail.com>
2016-03-23 17:26:16 +0800

    Add validation only_one_final_thesis

--- app/models/thesis.rb
+++ app/models/thesis.rb
@@ -4,5 +4,21 @@ class Thesis < ActiveRecord::Base
   mount_uploader :slide, BooklistUploader
 
   validates_presence_of :title
-  validates :final, uniqueness: true
+
+  validate :only_one_final_thesis
+  scope :final, -> { where(final: true)}
+
+  protected
+  def only_one_final_thesis
+    return unless final?
+
+    matches = Thesis.final
+    if persisted?
+      matches = matches.where('id != ?', id)
+    end
+    if matches.exists?
+      errors.add(:final, '您已經選擇其他篇為最終論文題目。')
+    end
+  end
+
 end
